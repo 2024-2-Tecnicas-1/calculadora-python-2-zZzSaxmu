878fe8a6dc37099d0c962540272a39276ff910d9
DEF <sami.alzate.correa@gmail.com>
2024-08-22 08:33:06 -0500

    modulo y potencia py

--- src/calculadora.py
+++ src/calculadora.py
@@ -13,11 +13,16 @@ def calcular(numero1, numero2, operacion):
         case '/':
             # Operación división
             respuesta = numero1 / numero2
+        case '^':
+            respuesta = numero1 ** numero2
+        
+        case '%':
+            respuesta = numero1 % numero2
         case _:
             raise ValueError("Operación inválida.")
 
         # TODO: DEBES COLOCAR TU CÓDIGO AQUÍ# TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO, COMPROBANDO LOS SÍMBOLOS ^ Y %.
-
+        
     return respuesta
 
 if __name__ == '__main__':
